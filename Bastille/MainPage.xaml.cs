0e641ac7cae5de4449c2514529d2dca6323ea0ae
Daniel Ortiz <DOrtiz1011@gmail.com>
2016-04-04 20:13:55 -0400

    Part 1 and some of part 2 implemented
    Crude GUI to enter data
    Basic logic
    Crashes on new Uri()

---
+++ Bastille/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Bastille
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private UrlData UrlData;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            UrlData = new UrlData();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            UrlData.SaveUrl(UserTokenTextBox.Text, UrlTextBox.Text);
+        }
+    }
+}
---
+++ Bastille/UrlData.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bastille
+{
+    public class UrlData
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userToken"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public bool SaveUrl(string userToken, string url)
+        {
+            IsStringParameterValid("userToken", userToken);
+            IsStringParameterValid("url", url);
+
+            var saveSucessful = SaveToUserHash(userToken, url);
+
+            if (saveSucessful)
+            {
+                SaveToDomainHash(userToken, url);
+            }
+
+            return saveSucessful;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userToken"></param>
+        /// <returns></returns>
+        public IEnumerable<string> GetUrlsForUser(string userToken)
+        {
+            IsStringParameterValid("userToken", userToken);
+
+            var urlsForUser = default(IEnumerable<string>);
+
+            if (UserHash.ContainsKey(userToken))
+            {
+                urlsForUser = UserHash[userToken];
+            }
+
+            return urlsForUser;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userToken"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public bool RemoveUrl(string userToken, string url)
+        {
+            IsStringParameterValid("userToken", userToken);
+            IsStringParameterValid("url", url);
+
+            var deleteSucessful = RemoveUrlFromUserHash(userToken, url);
+
+            if (deleteSucessful)
+            {
+                RemoveUrlFromDomainHash(userToken, url);
+            }
+
+            return deleteSucessful;
+        }
+
+        public IEnumerable<string> GetUsersByDomain(string domain)
+        {
+            IsStringParameterValid("domain", domain);
+
+            var usersByDomain = default(IEnumerable<string>);
+
+            return usersByDomain;
+        }
+
+        #endregion Public Methods
+
+        #region Private Fields
+
+        private Dictionary<string, List<string>> _userHash;
+        private Dictionary<string, List<string>> _domainHash;
+
+        #endregion Private Fields
+
+        #region Properties
+
+        private Dictionary<string, List<string>> UserHash
+        {
+            get
+            {
+                if (_userHash == null)
+                {
+                    _userHash = new Dictionary<string, List<string>>();
+                }
+
+                return _userHash;
+            }
+        }
+
+        private Dictionary<string, List<string>> DomainHash
+        {
+            get
+            {
+                if (_domainHash == null)
+                {
+                    _domainHash = new Dictionary<string, List<string>>();
+                }
+
+                return _domainHash;
+            }
+        }
+
+        #endregion Properties
+
+        #region Private Methods
+
+        private bool SaveToUserHash(string userToken, string url)
+        {
+            var saveSucessful = false;
+
+            if (UserHash.ContainsKey(userToken))
+            {
+                // user already exists in the hash, add url to the user's list
+                var urlList = UserHash[userToken];
+
+                if (!urlList.Contains(url))
+                {
+                    urlList.Add(url);
+
+                    urlList = urlList.OrderBy(x => x).ToList();
+                    saveSucessful = true;
+                }
+            }
+            else
+            {
+                // user does not exist in the hash, add the user, init a new list, then add the url to the list
+                var urlList = new List<string>();
+
+                urlList.Add(url);
+                UserHash.Add(userToken, urlList);
+                saveSucessful = true;
+            }
+
+            return saveSucessful;
+        }
+
+        private void SaveToDomainHash(string userToken, string url)
+        {
+            var domain = GetDomain(url);
+
+            if (DomainHash.ContainsKey(domain))
+            {
+                // user already exists in the hash, add url to the user's list
+                var userList = DomainHash[domain];
+
+                if (!userList.Contains(userToken))
+                {
+                    userList.Add(userToken);
+
+                    userList = userList.OrderBy(x => x).ToList();
+                }
+            }
+            else
+            {
+                // user does not exist in the hash, add the user, init a new list, then add the url to the list
+                var userList = new List<string>();
+
+                userList.Add(userToken);
+                DomainHash.Add(domain, userList);
+            }
+        }
+
+        private bool RemoveUrlFromUserHash(string userToken, string url)
+        {
+            var deleteSucessful = false;
+
+            if (UserHash.ContainsKey(userToken))
+            {
+                var urlList = UserHash[userToken];
+
+                if (urlList.Contains(url))
+                {
+                    urlList.Remove(url);
+                    deleteSucessful = true;
+
+                    if (urlList.Count == 0)
+                    {
+                        UserHash.Remove(userToken);
+                    }
+                }
+            }
+
+            return deleteSucessful;
+        }
+
+        private void RemoveUrlFromDomainHash(string userToken, string url)
+        {
+            var domain = GetDomain(url);
+            var domainUserList = DomainHash[domain];
+            var userUrlList = UserHash.ContainsKey(userToken) ? UserHash[userToken] : null;
+
+            if (userUrlList == null || userUrlList.Count == 0 || !userUrlList.Any(x => x.Contains(domain)))
+            {
+                domainUserList.Remove(userToken);
+            }
+        }
+
+        private void IsStringParameterValid(string parameterName, string parameterValue)
+        {
+            if (string.IsNullOrEmpty(parameterValue))
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+
+        private string GetDomain(string url)
+        {
+            var uri = new Uri(url);
+            string host = uri.Host;
+            return host.Replace("www.", string.Empty);
+        }
+
+        #endregion Private Methods
+    }
+}
